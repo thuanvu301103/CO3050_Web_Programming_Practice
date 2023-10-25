6658d1b5c69861ca85d008a6a0a64b6e255640bb
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-10-25 15:09:53 +0700

    Add files via upload

---
+++ LAB 03 - PHP & MySQL/Part 1/phan1_bai3/index_a.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phan 1 bai 3a</title>
+</head>
+<body>
+    <?php 
+        for ($i = 0; $i < 101; $i++) {
+            if ($i % 2 != 0) {
+                echo $i . "<br>";
+            }
+        } 
+    ?>
+</body>
+</html>
---
+++ LAB 03 - PHP & MySQL/Part 1/phan1_bai3/index_b.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phan 1 bai 3b</title>
+</head>
+<body>
+    <?php
+        $i = 0;
+        while ($i < 101) {
+            if ($i % 2 != 0) {
+                echo $i . "<br>";
+            }
+            $i++;
+        }
+    ?>
+</body>
+</html>
