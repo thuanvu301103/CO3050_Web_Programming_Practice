f772dbfbf0d13595a2a81001cbb2eeafbe3ba29a
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-10-25 15:08:48 +0700

    Add files via upload

---
+++ LAB 03 - PHP & MySQL/Part 1/phan1_bai1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phan 1 bai 1</title>
+</head>
+<body>
+    <?php
+        $x = 10;
+        $y = 7;
+        echo "<div>" . $x . " + " . $y . " = " . $x + $y . "</div>";
+        echo "<div>" . $x . " - " . $y . " = " . $x - $y . "</div>";
+        echo "<div>" . $x . " * " . $y . " = " . $x * $y . "</div>";
+        echo "<div>" . $x . " / " . $y . " = " . $x / $y . "</div>";
+        echo "<div>" . $x . " % " . $y . " = " . $x % $y . "</div>";
+    ?>
+</body>
+</html>
